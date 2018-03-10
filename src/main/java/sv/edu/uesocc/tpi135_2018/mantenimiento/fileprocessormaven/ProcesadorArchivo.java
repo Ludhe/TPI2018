b446f1091ff9a3bdc1f6fed836962d0e5bcfb0b8
doratt <doratt@doratt>
2018-03-10 13:06:41 -0600

    Cambio tipo archivo que recibe el metodo de parse

--- src/main/java/sv/edu/uesocc/tpi135_2018/mantenimiento/fileprocessormaven/ProcesadorArchivo.java
+++ src/main/java/sv/edu/uesocc/tpi135_2018/mantenimiento/fileprocessormaven/ProcesadorArchivo.java
@@ -20,53 +20,48 @@ import static java.util.stream.Collectors.toList;
  * @author zaldivar
  */
 public class ProcesadorArchivo {
-    
-private Path absolutePath;
-    
-private static final String REGEX="((?=[^\"']|\"[^\"]*\")+$)";
-    
-private boolean validarPath(final String path){
-  
-    if(path!=null&&!path.trim().isEmpty()){
-       File file=new File(path);
-       
-       return file.exists()&&file.isDirectory();
+
+    private Path absolutePath;
+
+    private static final String REGEX = "((?=[^\"']|\"[^\"]*\")+$)";
+
+    private boolean validarPath(final String path) {
+
+        if (path != null && !path.trim().isEmpty()) {
+            File file = new File(path);
+
+            return file.exists() && file.isDirectory();
+        }
+        return false;
     }
-    return false;
-}
 
-public List<Object> hasRequiredExtensions(final String path ) throws IOException{
-   List<Object> lista=new ArrayList<>();
-   if(path!=null&&!path.isEmpty()){
-       this.absolutePath=Paths.get(path);
-   lista=Files.walk(absolutePath).
-           filter(a -> validarPath(path)).
-           filter(a -> a.getFileName().toString().endsWith(".csv")).
-           collect(toList());
-   }
-           return lista;
-}
+    public List<Object> hasRequiredExtensions(final String path) throws IOException {
+        List<Object> lista = new ArrayList<>();
+        if (path != null && !path.isEmpty()) {
+            this.absolutePath = Paths.get(path);
+            lista = Files.walk(absolutePath).
+                    filter(a -> validarPath(path)).
+                    filter(a -> a.getFileName().toString().endsWith(".csv")).
+                    collect(toList());
+        }
+        return lista;
+    }
 
-public List<List<List<String>>> parser(List<String> paths, boolean saltarLinea,String separador) throws IOException{
-    List<List<String>> listaGeneral= new ArrayList<>();
-    List<List<List<String>>> listaMayor = new ArrayList<>();
-    if(!paths.isEmpty()){
-        for (String path : paths) {
+    public List<List<String>> parser(String path, boolean saltarLinea, String separador) throws IOException {
+        List<List<String>> listaGeneral = new ArrayList<>();
+        List<List<List<String>>> listaMayor = new ArrayList<>();
+        if (path != null && !path.isEmpty()) {
             Path pathArchivo = Paths.get(path);
-        if(Files.isReadable(pathArchivo)){
-        Files.lines(pathArchivo).
-                skip(saltarLinea?1:0).
-                filter(l -> l.contains(separador)).
-                forEach((f)->{
-                    listaGeneral.add(Arrays.asList(f.split(separador.concat(REGEX))));
-                });
+            if (Files.isReadable(pathArchivo)) {
+                Files.lines(pathArchivo).
+                        skip(saltarLinea ? 1 : 0).
+                        filter(l -> l.contains(separador)).
+                        forEach((f) -> {
+                            listaGeneral.add(Arrays.asList(f.split(separador.concat(REGEX))));
+                        });
+            }
         }
-        listaMayor.add(listaGeneral);
-        }
+        return listaGeneral;
     }
-    return listaMayor;
-}
 
-
-    
 }
