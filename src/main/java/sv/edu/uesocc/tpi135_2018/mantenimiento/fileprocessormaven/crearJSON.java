c8e13d2b3a9fd0086ba023c94ab3742229d0db71
katiro <katirox9@gmail.com>
2018-03-13 20:45:35 -0600

    Aplicacion Json

--- src/main/java/sv/edu/uesocc/tpi135_2018/mantenimiento/fileprocessormaven/crearJSON.java
+++ src/main/java/sv/edu/uesocc/tpi135_2018/mantenimiento/fileprocessormaven/crearJSON.java
@@ -1,9 +1,11 @@
 package sv.edu.uesocc.tpi135_2018.mantenimiento.fileprocessormaven;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import org.json.JSONObject;
 import org.json.JSONArray;
+import sv.edu.uesocc.tpi135_2018.mantenimiento.definiciones.Bitacora;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -17,7 +19,6 @@ import org.json.JSONArray;
 public class crearJSON {
 
     public static void main(String args[]) throws IOException {
-        JSONArray jsArray = new JSONArray();
         ProcesadorArchivo arc = new ProcesadorArchivo();
         List<List<String>> lista;
         String[] encabezado = new String[]{"N°", "Historico Mantenimientos", "N° Inventario", "Marca", "N° de Serie", "Modelo",
@@ -25,17 +26,30 @@ public class crearJSON {
 
         lista = arc.parser("src/main/resources/Sin-título-1.csv", false, ",");
 
-        for (int i = 0; i < lista.size(); i++) {
-            JSONObject jsObj = new JSONObject();
-
-            for (int j = 0; j < lista.get(i).size(); j++) {
-                jsObj.put(encabezado[j], lista.get(i).get(j));
-            }
-            jsArray.put(jsObj);
+        ArrayList<Bitacora> listB = new ArrayList<>();
+        Bitacora obj = new Bitacora(0, "shobe se la come", "numeroInventario", "marca", "numeroSerie", "modelo", "responsable", "sistemaOperativo", "version", true, "observaciones");
+        Bitacora obj1 = new Bitacora(0, "kevin se la come", "numeroInventario", "marca", "numeroSerie", "modelo", "responsable", "sistemaOperativo", "version", true, "observaciones");
+        Bitacora obj2 = new Bitacora(0, "chepe se la come", "numeroInventario", "marca", "numeroSerie", "modelo", "responsable", "sistemaOperativo", "version", true, "observaciones");
+        listB.add(obj);
+        listB.add(obj1);
+        listB.add(obj2);
+        JSONArray jsArray = new JSONArray();
+        JSONObject jsObj;
+        for (int i = 0; i < listB.size(); i++) {
+            jsObj = new JSONObject(listB.get(i));
             System.out.println(jsObj);
-
+            jsArray.put(jsObj);
         }
         System.out.println(jsArray);
+
+        jsObj = new JSONObject();
+        jsObj.put("tipo", "Historial");
+        jsObj.put("Registros", jsArray);
+        System.out.println(jsObj);
+        jsArray = new JSONArray();
+        jsArray.put(jsObj);
+        System.out.println(jsArray);
+
     }
 
 }
